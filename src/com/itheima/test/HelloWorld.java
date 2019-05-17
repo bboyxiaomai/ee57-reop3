db70df597369786874ff8bf53436d60fdb0b5ee8
xiaomai <1272692297@qq.com>
2019-05-17 08:35:00 +0800

    第2次修改

--- src/com/itheima/test/HelloWorld.java
+++ src/com/itheima/test/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("HelloWorld2");
         System.out.println("HelloWorld3");
         System.out.println("HelloWorld4");
+        System.out.println("嘻嘻嘻嘻嘻嘻");
     }
 }
